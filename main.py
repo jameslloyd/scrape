51f86450119f98ebf8f0a798d995d976f7fe569f
James Lloyd <james.lloyd@gmail.com>
2023-05-18 15:18:23 +0100

    threading

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from config import Config
-import random, requests, logging
+import random
+import requests
+import logging
+import threading
 
 logging.basicConfig(format=Config.LOGGING_FORMAT,level=Config.LOGGING_LEVEL,datefmt=Config.LOGGING_DATEFMT)
 
+def split_list(list_a, chunk_size):
+    for i in range(0, len(list_a), chunk_size):
+        yield list_a[i:i + chunk_size]
 
 def get_html(url, proxy = Config.PROXY_ENABLE):
     
@@ -13,16 +19,42 @@ def get_html(url, proxy = Config.PROXY_ENABLE):
     if proxy:
         proxy = random.choice(Config.PROXYS)
         if proxy == '':
-            proxy = False
+            proxyh = False
         else:
             proxyh = {"http":proxy,"https":proxy}
     useragent = random.choice(useragents)
     headers = {"User-Agent": useragent}
     output = requests.get(url, headers=headers,proxies=proxyh)
     logging.info(f'requesting {url} with useragent {useragent} and proxy {proxy}')
+    print(output.text)
     return output
     
-    
+def thread_jobs(jobs,noofthreads,function):
+    # jobs should be a list of urls  
+    # threads is the number of concurrent threads
+    jobgroups = [jobs[x:x+noofthreads] for x in range(0, len(jobs), noofthreads)]
+    for group in jobgroups:
+        threads = list() 
+        if len(group) < noofthreads:
+            # last group wont nessecarily be full
+            noofthreads = len(group)
+        for index in range(noofthreads):
+            url = str(group[index])
+            logging.info(f"Starting Thread {index} for event id {group[index]}.")
+            x = threading.Thread(target=function, args=(url,))
+            threads.append(x)
+            x.start()
+        
+        for index, thread in enumerate(threads):
+            logging.debug(f"Starting grab {group[index]} ")
+            thread.join()
+            logging.debug(f"Completed {group[index]}")             
+
+
 if __name__ == "__main__":
-    html = get_html('http://icanhazip.com')
-    print(html.text)
+    #html = get_html('http://icanhazip.com')
+    #print(html.text)
+    
+    
+    listofjobs = ['http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com','http://icanhazip.com']
+    thread_jobs(listofjobs,10,get_html)
